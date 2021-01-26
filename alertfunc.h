6d54a9b268f907116bbc29bbf680a6da06c3891e
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-01-26 15:47:21 +0530

    Update alertfunc.h

--- alertfunc.h
+++ alertfunc.h
@@ -3,6 +3,10 @@
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
 
+typedef void (*alerter_funcptr)();
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+
 // Alert functions
 void emailAlerter();
 void ledAlerter();
