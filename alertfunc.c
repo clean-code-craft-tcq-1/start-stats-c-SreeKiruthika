262e6b5fb301ac89135ef5c93ad8e466ad353bcc
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-01-26 15:42:16 +0530

    Create alertfunc.c
    New file for alert func

---
+++ alertfunc.c
@@ -0,0 +1,31 @@
+#include "alertfunc.h"
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    if ( computedStats.average > maxThreshold)
+    {
+        int num_func = 2;
+        for(int i = 0 ; i < num_func ; i++)
+        {
+            alerters[i]();
+        }
+    }
+}
+
+void emailAlerter()
+{
+    /*Increase the Email alert call count */
+    if (emailAlertCallCount < MAXCOUNT)
+    {
+        ++emailAlertCallCount;
+    }
+}   
+
+void ledAlerter() 
+{
+    /*Increase the LED alert call count */
+    if(ledAlertCallCount < MAXCOUNT)
+    {
+        ++ledAlertCallCount;
+    }
+}
