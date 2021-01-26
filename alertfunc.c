7ba7ac2fdbeddc0c4f2e905b6751f50dfb5936ed
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-01-26 15:48:20 +0530

    Update alertfunc.c

--- alertfunc.c
+++ alertfunc.c
@@ -1,5 +1,8 @@
 #include "alertfunc.h"
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0 ;
+
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
     if ( computedStats.average > maxThreshold)
